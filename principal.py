b26eff03f996606f16866fb40162ea362aea5581
Andres Velasco <afvelasco@gmai.com>
2025-08-05 14:47:04 -0500

    Se actualiza enunciado de trabajo a realizar

--- principal.py
+++ principal.py
@@ -1,1 +1,11 @@
-print("Hola mundo cruel y despiadado, duro con ellos!!!")
+"""
+Se trata de escribir un programa de computador que permita almacenar
+una lista de productos (codigo, nombre, precio, stock). El programa 
+debe ofrecer opciones para agregar un nuevo producto, buscar la infor-
+mación de un producto a partir de su código, modificar un producto,
+borrar un producto, listar todos los productos. El programa debe manejar
+persistencia para los datos almacenados, utilizando MySQL o utilizando 
+archivos planos con JSON
+
+NOTA: Utilizar POO
+"""
